09c07824c70feb19d16869a13ffa018ad6214712
Martin Temmink <martin.temmink@gmail.com>
2014-11-09 13:40:33 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -88,5 +88,16 @@ static const int GRID_COLUMNS = 10;
     creature.isAlive = !creature.isAlive;
 }
 
-
+- (void)evolveStep
+{
+    //update each Creature's neighbor count
+    [self countNeighbors];
+    
+    //update each Creature's state
+    [self updateCreatures];
+    
+    //update the generation so the label's text will display the correct generation
+    _generation++;
+    
+}
 @end
