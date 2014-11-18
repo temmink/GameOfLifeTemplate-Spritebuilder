ee6666cb4422bced1d1dfab6fb99f2eca14025e5
Martin Temmink <martin.temmink@gmail.com>
2014-11-18 10:11:24 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -87,7 +87,7 @@ static const int GRID_COLUMNS = 10;
     //invert it's state - kill it if it's alive, bring it to life if it's dead.
     creature.isAlive = !creature.isAlive;
 }
-
+}
 
 - void()updateCreatures
 {
